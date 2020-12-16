fd66f83086c949d730d5f4f69b723901fa46bab1
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-12-15 17:56:46 -0800

    login auth post route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 
 
@@ -39,7 +41,19 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        res.send('user saved');
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), { 
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
 
      } catch (err) {
          console.error(err.message);
