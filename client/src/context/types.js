0fe67a103735ae8340ce341170cca3ef094075fe
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-12-18 14:56:28 -0800

    context api started for contacts/navbar component/and Front end routes

---
+++ client/src/context/types.js
@@ -0,0 +1,9 @@
+export const ADD_CONTACT = 'ADD-CONTACT';
+export const DELETE_CONTACT = 'DELETE-CONTACT';
+export const SET_CURRENT = 'SET-CURRENT';
+export const CLEAR_CURRENT = 'CLEAR-CURRENT';
+export const UPDATE_CONTACT = 'UPDATE_CONTACT';
+export const FILTER_CONTACTS = 'FILTER_CONTACTS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVER_ALERT = 'REMOVE_ALERT';
